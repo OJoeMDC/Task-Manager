281b19775be21da627f9d74dfcf6e2332fb0b321
Osceola Martin del Campo <ojoe.mdc@gmail.com>
2026-06-06 21:10:00 -0500

    added app.list(PORT);

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const db = require('./database');
 const cors = require('cors');
+
+
 const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 const allowedOrigins = [
     'http://localhost:5173',
