d9a8770d30080da84389d03005d7113e1ef994fb
Osceola Martin del Campo <ojoe.mdc@gmail.com>
2026-06-15 17:13:04 -0500

    removed redundant app.listen

--- server/index.js
+++ server/index.js
@@ -30,9 +30,6 @@ app.use(cors({
 
 console.log('process.env.PORT =', process.env.PORT);
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const DB_PATH = process.env.DB_PATH ?? (process.env.NODE_ENV == 'production' ? '/data/tasks.db' : path.join(__dirname, 'tasks.db'));
