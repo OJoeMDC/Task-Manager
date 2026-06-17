96d86f9f51fa6a27b5a2a3e5cc0a619976e7a066
Osceola Martin del Campo <ojoe.mdc@gmail.com>
2026-06-16 22:26:09 -0500

    added animated Task Manager title

--- client/src/Logo.jsx
+++ client/src/Logo.jsx
@@ -2,9 +2,36 @@ import { useEffect, useState } from "react";
 import "./Logo.css";
 
 export default function Logo() {
-    const [text, setText] = useState("Task Manager");
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(true);
+    const fullWord = !isCorrect ? "Task Managr" : "Task Manager";
+    const [text, setText] = useState(fullWord);
 
-    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (isDeleting) {
+                const currentText = text.slice(0, text.length - 1);
+
+               setText(currentText);
+
+               if (currentText === "Task Mana") {
+                setIsDeleting(false);
+               }
+            } else {
+
+                const currentText = fullWord.slice(0, text.length + 1);
+
+                setText(currentText);
+
+                if (currentText === fullWord) {
+                    setIsDeleting(true);
+                    setIsCorrect(prev => !prev);
+                }
+            }
+        }, 500);
+        return () => clearInterval(interval);
+    }, [text, isDeleting, fullWord]);
+
     return (
         <span className="logo">
             {text}
