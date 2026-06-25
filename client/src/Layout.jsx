cabf0a1c9e01970e93c8787ccfbc2162d1b0764c
Osceola Martin del Campo <ojoe.mdc@gmail.com>
2026-06-25 12:00:54 -0500

    More mobile responsive testing

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -5,11 +5,13 @@ import { Outlet } from "react-router-dom";
 function Layout({ user, setUser }) {
     return(
         <div className="app-layout">
+            <Navbar user={user} setUser={setUser} />
 
             <main className="page">
                 <Outlet />
             </main>
 
+            <Footer />
         </div>
     )
 }
